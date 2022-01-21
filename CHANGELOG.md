afb735d628ba5c5e4f9523108c507fb66be92f7f
etiennedg <dussert.girard@gmail.com>
2022-01-21 17:06:17 +0100

    minor adjustements

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,10 @@
 ## [0.0.1]
 
 - Initial release
+
+## [0.0.2]
+
+- Small adjustements including :
+    - not tagging directly Creators and Contributors to avoid unwanted notifications
+    - add a link to the info message after creating/updating a database entry
+    
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -2,7 +2,6 @@
 /* eslint-disable-next-line @typescript-eslint/naming-convention */
 export type TagsType = { multi_select: any[] };
 export type NumberType = { number: number };
-export type PeopleType = { people: { id: string }[] };
 
 /* CONTEXT */
 export type EditorContext = { path: string | null, selection: string | null };
